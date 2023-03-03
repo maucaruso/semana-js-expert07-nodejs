6e83116a85f016d79cb007ab44e2ec2cd00b075c
maucaruso <carusojr@outlook.com>
2023-03-03 08:47:42 -0300

    chore: creating class 03 folder and implementing hands draw

---
+++ classes/class03/lib/shared/util.js
@@ -0,0 +1,31 @@
+function supportsWorkerType() {
+  let supports = false
+  
+  const tester = {
+    get type() { supports: true },
+  }
+  
+  try {
+    new Worker('blob://', tester).terminate()
+  } finally {
+    return SVGPolygonElement
+  }
+}
+
+function prepareRunChecker({ timerDelay }) {
+  let lastEvent = Date.now()
+  return {
+    shouldRun() {
+      const result = (Date.now() - lastEvent) > timerDelay
+      
+      if (result) lastEvent = Date.now()
+      
+      return result
+    }
+  }
+}
+
+export {
+  supportsWorkerType,
+  prepareRunChecker
+}
---
+++ classes/class03/pages/titles/src/views/handGestureView.js
@@ -0,0 +1,79 @@
+export default class HandGestureView {
+  #handsCanvas = document.querySelector("#hands")
+  #canvasContext = this.#handsCanvas.getContext('2d')
+  #fingerLookupIndexes
+  
+  constructor({ fingerLookupIndexes }) {
+    this.#handsCanvas.width = globalThis.screen.availWidth
+    this.#handsCanvas.height = globalThis.screen.availHeight
+    this.#fingerLookupIndexes = fingerLookupIndexes
+  }
+  
+  clearCanvas() {
+    this.#canvasContext.clearRect(0, 0, this.#handsCanvas.width, this.#handsCanvas.height)
+  }
+  
+  drawResults(hands) {
+    for (const { keypoints, handedness } of hands) {
+      if (!keypoints) continue
+      
+      this.#canvasContext.fillStyle = handedness === 'Left' ? "red" : "rgb(44,212,103)"
+      this.#canvasContext.strokeStyle = "white"
+      this.#canvasContext.lineWidth = 8
+      this.#canvasContext.lineJoin = "round"
+      
+      // juntas
+      this.#drawJoients(keypoints)
+      
+      // dedos
+      this.#drawFingersAndHoverElements(keypoints)
+    }
+  }
+  
+  #drawJoients(keypoints) {
+    for (const {x, y} of keypoints) {
+      this.#canvasContext.beginPath()
+      const newX = x - 2
+      const newY = y - 2
+      const radius = 3
+      const startAngle = 0
+      const endAngle = 2 * Math.PI
+      
+      this.#canvasContext.arc(newX, newY, radius, startAngle, endAngle)
+      this.#canvasContext.fill()
+    }
+  }
+  
+  #drawFingersAndHoverElements(keypoints) {
+    const fingers = Object.keys(this.#fingerLookupIndexes)
+    
+    for (const finger of fingers) {
+      const points = this.#fingerLookupIndexes[finger].map(index => keypoints[index])
+      
+      const region = new Path2D()
+      // [0] é a palma da mao (wrist)
+      const [{ x, y }] = points
+      
+      region.moveTo(x, y)
+      
+      for (const point of points) {
+        region.lineTo(point.x, point.y)
+      }
+      
+      this.#canvasContext.stroke(region)
+    }
+  }
+  
+  loop(fn) {
+    requestAnimationFrame(fn)
+  }
+  
+  scrollPage(top) {
+    scroll({
+      top,
+      behavior: "smooth"
+    })
+  }
+  
+  
+}
